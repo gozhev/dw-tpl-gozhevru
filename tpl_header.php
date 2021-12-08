163c967444a43c809a712f60c2fe91c9c32e06bc
Mikhail Gozhev <m@gozhev.ru>
2021-12-09 02:28:12 +0300

    initial commit

---
+++ tpl_header.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('DOKU_INC')) die();
+?>
+
+<div id="dokuwiki__header">
+  <div class="pad group">
+    <a class="logo" href="/">Memex</a>
+    <a class="back" href="https://m.gozhev.ru">back</a>
+    <a class="menu" href="/admin">menu</a>
+  </div>
+</div>
